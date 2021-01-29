a019a176fb3ab81e821596dbdc943e3873643c72
Miguel Rosas <68622478+NonExxy@users.noreply.github.com>
2021-01-29 09:23:57 -0700

    scss inclusion

--- start-project.sh
+++ start-project.sh
@@ -14,6 +14,9 @@ if [[ ${projecttype} == "website" ]]
 then
     cp ./automation/filetemplates/template.html ./${projectname}/home.html
     mkdir ./${projectname}/css
+    mkdir ./${projectname}/scss
+    touch ./${projectname}/scss/styles.scss
+    echo "/*Remember to put 'sass --watch scss:css' from the terminal while in this directory*/" >> ./${projectname}/scss/styles.scss
     cp ./automation/filetemplates/template.css ./${projectname}/css/styles.css
     cp ./automation/filetemplates/template.js ./${projectname}/main.js
 elif [[${projecttype} == "shell" ]]
